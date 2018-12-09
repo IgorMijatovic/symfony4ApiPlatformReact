62d32ea855cfb77f3ecb3d0b6db516ec337b8534
igor <igor.mijatovic@outlook.de>
2018-12-09 14:20:01 +0100

    different user view for admins with decorated usercontextbuilder

---
+++ src/Serializer/UserContextBuilder.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Serializer;
+
+use ApiPlatform\Core\Exception\RuntimeException;
+use ApiPlatform\Core\Serializer\SerializerContextBuilderInterface;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class UserContextBuilder implements SerializerContextBuilderInterface
+{
+    /**
+     * @var SerializerContextBuilderInterface
+     */
+    private $decorated;
+    /**
+     * @var AuthorizationCheckerInterface
+     */
+    private $authorizationChecker;
+
+    /**
+     * UserContextBuilder constructor.
+     * @param SerializerContextBuilderInterface $decorated
+     * @param AuthorizationCheckerInterface $authorizationChecker
+     */
+    public function __construct(SerializerContextBuilderInterface $decorated, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->decorated = $decorated;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * @param Request $request
+     * @param bool $normalization
+     * @param array|null $extractedAttributes
+     * @return array
+     */
+    public function createFromRequest(Request $request, bool $normalization, array $extractedAttributes = null): array
+    {
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes );
+
+        // class being serialized/deserialized
+        $resourceClass = $context['resource_class'] ?? null;
+
+        if (
+            User::class === $resourceClass &&
+            isset($context['groups']) &&
+            $normalization === true &&
+            $this->authorizationChecker->isGranted(USer::ROLE_ADMIN)
+        ) {
+            $context['groups'][] = 'get-admin';
+        }
+
+        return $context;
+    }
+}
