7de8be98331b9e550d22758f72c64225f0d8b1b8
igor <igor.mijatovic@outlook.de>
2018-12-21 16:00:51 +0100

    partial pagination perf. gain & configuring collection pagination

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -11,7 +11,11 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
  * @ApiResource(
- *     attributes={"order"={"published":"DESC"}},
+ *     attributes={
+ *          "order"={"published":"DESC"},
+ *          "pagination_client_enabled": true,
+ *          "pagination_client_items_per_page": true
+ *     },
  *     itemOperations={
  *          "get",
  *          "put"={
