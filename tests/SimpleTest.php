cc6e63921bba3674457411f621d878f01cccbf54
igor <igor.mijatovic@outlook.de>
2018-12-25 13:47:21 +0100

    installing phpunit and writing first basic test

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testAddition()
+    {
+        $this->assertEquals(5, 2+3, 'Five was expected');
+    }
+}
