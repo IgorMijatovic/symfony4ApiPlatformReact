a5892aa2c4cec81c3e6ef5b5d885f242ed67ba55
igor <igor.mijatovic@outlook.de>
2018-12-25 14:27:24 +0100

    Mock(stubs) part 1 mocking dependencies, entities

--- tests/EventSubscriber/AuthoredEntitySubscriberTest.php
+++ tests/EventSubscriber/AuthoredEntitySubscriberTest.php
@@ -3,9 +3,12 @@ namespace App\Tests\EventSubscriber;
 
 
 use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
 use App\EventSubscriber\AuthoredEntitySubscriber;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 class AuthoredEntitySubscriberTest extends TestCase
 {
@@ -17,5 +20,21 @@ class AuthoredEntitySubscriberTest extends TestCase
             ['getAuthenticatedUser', EventPriorities::PRE_WRITE],
             $result[KernelEvents::VIEW]
         );
+    }
+
+    public function testSetAuthorCall()
+    {
+        $tokenMock = $this->getMockBuilder(TokenInterface::class)
+            ->getMockForAbstractClass();
+        $tokenMock->expects($this->once())
+            ->method('getUser')
+            ->willReturn(new User());
+
+        $tokenStorageMock = $this->getMockBuilder(TokenStorageInterface::class)
+            ->getMockForAbstractClass();
+
+        $tokenStorageMock->expects($this->once())
+            ->method('getToken')
+            ->willReturn($tokenMock);
     }
 }
